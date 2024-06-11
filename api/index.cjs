521560053a6e9101e139bdc623147d98afe6ddd8
Gabriel Francisco <gab9244@gmail.com>
2024-06-11 17:35:17 -0300

    ...

--- api/index.cjs
+++ api/index.cjs
@@ -41,7 +41,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://theblog-4agb.onrender.com');
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
